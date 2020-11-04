7f18faba7415fd7fdece1c08537e8f38860afd4c
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-04 17:34:30 +0530

    store is subscribe and then the dispatch called

--- asyncActions.js
+++ asyncActions.js
@@ -74,3 +74,5 @@ const fetchUsers = () => {
 }
 
 const store = createStore(reducer, applyMiddleware(thunkMiddleware))
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(fetchUsers())
